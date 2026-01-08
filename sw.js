dacc69f2d35e1dcf95889db0f7f131d5867cf4ca
GuillaumeG <azazel451@gmail.com>
2026-01-08 22:18:08 +0100

    feat : add annotate json output

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'media-gallery-v2';
+const CACHE_NAME = 'media-gallery-v4';
 const ASSETS = [
     './',
     './index.html',
@@ -6,9 +6,26 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    // Force the waiting service worker to become the active service worker.
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    // Delete old caches
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
     );
 });
 
